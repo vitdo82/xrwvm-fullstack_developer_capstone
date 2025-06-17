f3d5ed18833a291e0252714ae2a35a5a404b383e
Vitali Rydvan <7825134@gmail.com>
2025-06-17 21:07:20 +0200

    feat: add registration page

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,11 +1,14 @@
 # Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
+    path(route='login', view=views.login_user, name='login'),
+    path(route='logout', view=views.logout_request, name='logout'),
+    path(route='registration', view=views.registration, name='registration'),
     # # path for registration
 
     # path for login
